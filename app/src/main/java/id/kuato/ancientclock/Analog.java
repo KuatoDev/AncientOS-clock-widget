1d8c75851519730b6abd73339333f088e469f0dc
kuatodev <kuatodev@gmail.com>
2020-07-17 07:09:35 +0700

    Create Project

---
+++ app/src/main/java/id/kuato/ancientclock/Analog.java
@@ -0,0 +1,21 @@
+package id.kuato.ancientclock;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import id.kuato.ancientclock.R;
+public class Analog extends AppWidgetProvider{
+
+    public void onReceive(Context context, Intent intent)
+    {
+        String act=intent.getAction();
+        if(AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
+        {
+            RemoteViews analogclock =new RemoteViews(context.getPackageName(), R.layout.analog);
+            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), analogclock);
+        }
+    }
+
+}
