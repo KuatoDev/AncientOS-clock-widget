7cf0bac9ac4a153afa900bcbcab1951947008bc0
kuatwarsono@gmail.com <kuatwarsono@gmail.com>
2021-08-11 11:44:36 +0700

    Widget: Cleanup old style widget

--- app/src/main/java/id/kuato/ancientclock/a12digital.java
+++ app/src/main/java/id/kuato/ancientclock/a12digital.java
@@ -6,16 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import id.kuato.ancientclock.R;
-public class ChronoJunu extends AppWidgetProvider{
 
+public class a12digital extends AppWidgetProvider
+{
     public void onReceive(Context context, Intent intent)
     {
         String act=intent.getAction();
-        if(AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
         {
-            RemoteViews analogclock =new RemoteViews(context.getPackageName(), R.layout.chronograph_styled);
-            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), analogclock);
+            RemoteViews digitalclock =new RemoteViews(context.getPackageName(), R.layout.a12_digital);
+            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), digitalclock);
         }
     }
-
 }
