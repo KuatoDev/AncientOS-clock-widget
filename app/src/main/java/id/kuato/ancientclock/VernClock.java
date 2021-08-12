2632de028ebc3742cc6d2023a5b81508a8de08f6
KuatoDev <kuatwarsono@gmail.com>
2021-08-12 09:53:36 +0700

    rework digital clock and add new clock wodget

---
+++ app/src/main/java/id/kuato/ancientclock/VernClock.java
@@ -0,0 +1,20 @@
+package id.kuato.ancientclock;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import id.kuato.ancientclock.R;
+public class VernClock extends AppWidgetProvider
+{
+    public void onReceive(Context context, Intent intent)
+    {
+        String act=intent.getAction();
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
+        {
+            RemoteViews VernClock =new RemoteViews(context.getPackageName(), R.layout.vern_clock);
+            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), VernClock);
+        }
+    }
+}
