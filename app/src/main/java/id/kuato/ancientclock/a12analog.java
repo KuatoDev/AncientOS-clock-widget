11aa40c408f38e0caeb22997daca535e6c06ab74
KuatoDev <kuatwarsono@gmail.com>
2021-08-14 19:35:36 +0700

    analog: use vector drawable instead of png

--- app/src/main/java/id/kuato/ancientclock/a12analog.java
+++ app/src/main/java/id/kuato/ancientclock/a12analog.java
@@ -6,16 +6,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import id.kuato.ancientclock.R;
-public class a12analog extends AppWidgetProvider{
+public class a12analog extends AppWidgetProvider
+{
 
     public void onReceive(Context context, Intent intent)
     {
         String act=intent.getAction();
-        if(AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
         {
-            RemoteViews a12clock =new RemoteViews(context.getPackageName(), R.layout.a12_analog);
-            AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), a12clock);
+            RemoteViews a12clock = new RemoteViews(context.getPackageName(),
+                                                   R.layout.a12_analog);
+            AppWidgetManager.getInstance(context)
+                .updateAppWidget(intent.getIntArrayExtra
+                                 (AppWidgetManager.EXTRA_APPWIDGET_IDS), a12clock);
         }
     }
-
 }
