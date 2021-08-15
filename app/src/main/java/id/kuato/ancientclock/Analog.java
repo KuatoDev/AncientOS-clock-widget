6a4d12be48eb010483e15ee24ce635b1eecef93c
KuatoDev <kuatwarsono@gmail.com>
2021-08-16 00:08:56 +0700

    anc: merge iconpack

--- app/src/main/java/id/kuato/ancientclock/Analog.java
+++ app/src/main/java/id/kuato/ancientclock/Analog.java
@@ -6,16 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 import id.kuato.ancientclock.R;
-public class Analog extends AppWidgetProvider{
 
-    public void onReceive(Context context, Intent intent)
-    {
+public class Analog extends AppWidgetProvider {
+    public void onReceive(Context context, Intent intent) {
         String act=intent.getAction();
-        if(AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act))
-        {
+        if (AppWidgetManager.ACTION_APPWIDGET_UPDATE.equals(act)) {
             RemoteViews analogclock =new RemoteViews(context.getPackageName(), R.layout.analog);
             AppWidgetManager.getInstance(context).updateAppWidget(intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS), analogclock);
         }
     }
-
 }
+
